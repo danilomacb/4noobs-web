78153509eeab331d8e5d77484a42dc9d63d23a7f
danilomacb <danilo.bakun@gmail.com>
2020-06-25 12:18:00 -0300

    creating sidebar

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
+import SideBar from "./SideBar";
+
 function Container({ markdown }) {
   return (
     <main>
-      <ReactMarkdown source={markdown} escapeHtml={false} />
+      <SideBar />
+      <div id="markdown">
+        <ReactMarkdown source={markdown} escapeHtml={false} />
+      </div>
     </main>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>C++</li>
+        <li>Java</li>
+        <li>Elixir</li>
+        <li>C</li>
+        <li>Ruby</li>
+        <li>JavaScript</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default SideBar;
