e75a138c2919a9b59c005b2ec0c53ff35f7e62bb
danilomacb <danilo.bakun@gmail.com>
2020-06-27 11:21:19 -0300

    creating getTags

--- src/services/request.js
+++ src/services/request.js
@@ -1,10 +1,28 @@
 async function request(link) {
+  function getTags(markdown) {
+    //wip
+    let index = 0;
+
+    while (index !== -1) {
+      let imgPosition = markdown.indexOf("<img", index);
+
+      if (imgPosition !== -1) {
+        index = imgPosition + 1;
+      } else {
+        index = imgPosition;
+      }
+      console.log(imgPosition);
+    }
+  }
+
   try {
     let response = await fetch(link);
 
     response = await response.json();
     response = await fetch(response.download_url);
     response = await response.text();
+
+    getTags(response);
 
     return response;
   } catch (err) {
