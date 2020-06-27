13f7e7e23a4a8dd5ba6dc7022a824ebb966e1ef3
danilomacb <danilo.bakun@gmail.com>
2020-06-27 10:57:07 -0300

    creating dropdowns on sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import pages from "../pages.json";
 
 function SideBar() {
+  const [displayLanguages, setDisplayLanguages] = useState("none");
+  const [displayFrameworks, setDisplayFrameworks] = useState("none");
+
+  function show(category, set) {
+    if (category === "none") {
+      set("block");
+    } else {
+      set("none");
+    }
+  }
+
   return (
     <nav>
       <ul>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <Link to={page.path}>{page.name}</Link>
-          </li>
-        ))}
+        <li key={pages.home.name}>
+          <Link to={pages.home.path}>{pages.home.name}</Link>
+        </li>
+
+        <label onClick={() => show(displayLanguages, setDisplayLanguages)}>
+          Linguagens de programação
+        </label>
+        <ul style={{ display: displayLanguages }}>
+          {pages.programingLanguages.map((language) => (
+            <li key={language.name}>
+              <Link to={language.path}>{language.name}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <br />
+
+        <label onClick={() => show(displayFrameworks, setDisplayFrameworks)}>Frameworks</label>
+        <ul style={{ display: displayFrameworks }}>
+          {pages.frameworks.map((framework) => (
+            <li key={framework.name}>
+              <Link to={framework.path}>{framework.name}</Link>
+            </li>
+          ))}
+        </ul>
       </ul>
     </nav>
   );
