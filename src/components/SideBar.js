c1d126e01fd1c16be13e5aae4b9d7c452a7d4d27
danilomacb <danilo.bakun@gmail.com>
2020-06-28 15:53:00 -0300

    using sass in separated files

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "../styles/sideBar.scss";
 import pages from "../pages.json";
 
 function SideBar() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
+import "./styles/global.scss";
 import App from "./App";
 
 ReactDOM.render(
