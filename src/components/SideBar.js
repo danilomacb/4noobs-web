754ae22fc4c10f36e4ef1b23cd5e710ad46b3d47
danilomacb <danilo.bakun@gmail.com>
2020-07-01 17:40:09 -0300

    removing pages and routes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,11 +7,6 @@ import "../styles/sideBar.scss";
 import pages from "../pages.json";
 
 function SideBar() {
-  const [displayLanguages, setDisplayLanguages] = useState("none");
-  const [displayFrameworks, setDisplayFrameworks] = useState("none");
-  const [displayTools, setDisplayTools] = useState("none");
-  const [displayOS, setDisplayOS] = useState("none");
-  const [displayDesign, setDisplayDesign] = useState("none");
   const [displayModels, setDisplayModels] = useState("none");
 
   function show(categoryName, set) {
@@ -25,94 +20,8 @@ function SideBar() {
   return (
     <nav>
       <ul>
-        <li>
-          <Link id="home" to={pages.home.path}>
-            <img src={`${process.env.PUBLIC_URL}/images/he4rt.png`} alt="" />
-          </Link>
-        </li>
-
-        <li className="categoryName" onClick={() => show(displayLanguages, setDisplayLanguages)}>
-          Linguagens de programação
-          {displayLanguages === "none" ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
-        </li>
-        <ul className="categoryList" style={{ display: displayLanguages }}>
-          {pages.programingLanguages.map((language) => (
-            <li className="categoryItem" key={language.name}>
-              <Link to={language.path}>{language.name}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <li className="categoryName" onClick={() => show(displayFrameworks, setDisplayFrameworks)}>
-          Frameworks
-          {displayFrameworks === "none" ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
-        </li>
-        <ul className="categoryList" style={{ display: displayFrameworks }}>
-          {pages.frameworks.map((framework) => (
-            <li className="categoryItem" key={framework.name}>
-              <Link to={framework.path}>{framework.name}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <li className="categoryName" onClick={() => show(displayTools, setDisplayTools)}>
-          Ferramentas
-          {displayTools === "none" ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
-        </li>
-        <ul className="categoryList" style={{ display: displayTools }}>
-          {pages.tools.map((tool) => (
-            <li className="categoryItem" key={tool.name}>
-              <Link to={tool.path}>{tool.name}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <li className="categoryName" onClick={() => show(displayOS, setDisplayOS)}>
-          Sistemas operacionais
-          {displayOS === "none" ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
-        </li>
-        <ul className="categoryList" style={{ display: displayOS }}>
-          {pages.operationalSystems.map((os) => (
-            <li className="categoryItem" key={os.name}>
-              <Link to={os.path}>{os.name}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <li className="categoryName" onClick={() => show(displayDesign, setDisplayDesign)}>
-          Design
-          {displayDesign === "none" ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
-        </li>
-        <ul className="categoryList" style={{ display: displayDesign }}>
-          {pages.designs.map((design) => (
-            <li className="categoryItem" key={design.name}>
-              <Link to={design.path}>{design.name}</Link>
-            </li>
-          ))}
-        </ul>
-
         <li className="categoryName" onClick={() => show(displayModels, setDisplayModels)}>
-          Models
+          Model
           {displayModels === "none" ? (
             <FontAwesomeIcon icon={faChevronDown} />
           ) : (
@@ -120,11 +29,9 @@ function SideBar() {
           )}
         </li>
         <ul className="categoryList" style={{ display: displayModels }}>
-          {pages.models.map((model) => (
-            <li className="categoryItem" key={model.name}>
-              <Link to={model.path}>{model.name}</Link>
-            </li>
-          ))}
+          <li className="categoryItem" key={pages.model.name}>
+            <Link to={pages.model.path}>{pages.model.name}</Link>
+          </li>
         </ul>
       </ul>
     </nav>
--- src/routes.js
+++ src/routes.js
@@ -7,51 +7,12 @@ import Page from "./components/Page";
 function Routes() {
   return (
     <BrowserRouter>
-      <Route exact path={pages.home.path} component={() => <Page link={pages.home.link} />} />
-
-      {pages.programingLanguages.map((language) => (
-        <Route
-          key={language.name}
-          exact
-          path={language.path}
-          component={() => <Page link={language.link} />}
-        />
-      ))}
-
-      {pages.frameworks.map((framework) => (
-        <Route
-          key={framework.name}
-          exact
-          path={framework.path}
-          component={() => <Page link={framework.link} />}
-        />
-      ))}
-
-      {pages.tools.map((tool) => (
-        <Route key={tool.name} exact path={tool.path} component={() => <Page link={tool.link} />} />
-      ))}
-
-      {pages.operationalSystems.map((os) => (
-        <Route key={os.name} exact path={os.path} component={() => <Page link={os.link} />} />
-      ))}
-
-      {pages.designs.map((design) => (
-        <Route
-          key={design.name}
-          exact
-          path={design.path}
-          component={() => <Page link={design.link} />}
-        />
-      ))}
-
-      {pages.models.map((model) => (
-        <Route
-          key={model.name}
-          exact
-          path={model.path}
-          component={() => <Page link={model.link} />}
-        />
-      ))}
+      <Route
+        key={pages.model.name}
+        exact
+        path={pages.model.path}
+        component={() => <Page link={pages.model.link} />}
+      />
     </BrowserRouter>
   );
 }
