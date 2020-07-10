512b49b635ca97a2bdc87b91050f2ac393e1a697
danilomacb <danilo.bakun@gmail.com>
2020-07-10 07:55:18 -0300

    removing redux and redux saga

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import "./styles/global.scss";
-import configureStore from "./state/store";
 import App from "./App";
-
-const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
