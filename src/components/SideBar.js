92c7fd7b3eb617c511ec8e58552d38c22c1db44b
danilomacb <danilo.bakun@gmail.com>
2020-07-11 15:55:54 -0300

    adding more styles for sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/sideBar.scss";
 import links from "../4noobs.json";
 
 function SideBar() {
+  const [languagesDisplay, setLanguagesDisplay] = useState("none");
+  const [frameworksDisplay, setFrameworksDisplay] = useState("none");
+
+  function changeDisplay(category, set) {
+    if (category === "none") {
+      set("block");
+    } else {
+      set("none");
+    }
+  }
+
   return (
     <nav>
-      <ul>
-        <li>
+      <ul id="main-list">
+        <li id="home">
           <Link to="/">
             <img src={`${process.env.PUBLIC_URL}/images/he4rt.png`} alt="4noobs" />
           </Link>
         </li>
 
-        {links.programingLanguages.map((link) => (
-          <li key={link.name}>
-            <Link to={`/${link.user}/${link.repository}`}>{link.name}</Link>
-          </li>
-        ))}
+        <li
+          className="category"
+          onClick={() => changeDisplay(languagesDisplay, setLanguagesDisplay)}
+        >
+          Linguagens de Programação
+          {languagesDisplay === "none" ? (
+            <FontAwesomeIcon icon={faChevronDown} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronUp} />
+          )}
+        </li>
+        <ul className="category-list" style={{ display: languagesDisplay }}>
+          {links.programingLanguages.map((link) => (
+            <li className="category-item" key={link.name}>
+              <Link to={`/${link.user}/${link.repository}`}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
 
-        {links.frameworks.map((link) => (
-          <li key={link.name}>
-            <Link to={`/${link.user}/${link.repository}`}>{link.name}</Link>
-          </li>
-        ))}
+        <li
+          className="category"
+          onClick={() => changeDisplay(frameworksDisplay, setFrameworksDisplay)}
+        >
+          Frameworks
+          {frameworksDisplay === "none" ? (
+            <FontAwesomeIcon icon={faChevronDown} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronUp} />
+          )}
+        </li>
+        <ul className="category-list" style={{ display: frameworksDisplay }}>
+          {links.frameworks.map((link) => (
+            <li className="category-item" key={link.name}>
+              <Link to={`/${link.user}/${link.repository}`}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
 
-        <li>
+        <li id="model">
           <Link to={`/${links.model.user}/${links.model.repository}`}>{links.model.name}</Link>
         </li>
       </ul>
