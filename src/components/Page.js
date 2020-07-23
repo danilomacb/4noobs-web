a47b1e8fc55824b057dc1f81b9844f5f746a61ff
danilomacb <danilo.bakun@gmail.com>
2020-07-23 10:55:27 -0300

    react router link working only on markdown links

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown/with-html";
 import { Link } from "react-router-dom";
-import htmlParser from "react-markdown/plugins/html-parser";
 
 import getMarkdown from "../utils/getMarkdown";
 
@@ -22,38 +21,12 @@ function Page({ match }) {
     });
   }, [match]);
 
-  const customElements = {
-    "a": Link,
-  };
-
-  const parseHtml = htmlParser({
-    isValidNode: (node) => node.type !== "script",
-    processingInstructions: [
-      {
-        shouldProcessNode: (node) => {
-          // Process the node if it matches a custom element
-          return node.name && customElements[node.name];
-        },
-        processNode: (node) => {
-          let CustomElement = customElements[node.name];
-          return <CustomElement />;
-        },
-      },
-      // Default processing
-      {
-        shouldProcessNode: () => true,
-        processNode: htmlParser.processNodeDefinitions.processDefaultNode,
-      },
-    ],
-  });
-
   return (
     <main>
       <ReactMarkdown
         source={markdown}
         escapeHtml={false}
         renderers={{ link: RouterLink }}
-        astPlugins={[parseHtml]}
       />
     </main>
   );
