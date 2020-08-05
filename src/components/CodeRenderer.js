8c91d13c4a0efda9c1d9aa09b601378414b76919
danilomacb <danilo.bakun@gmail.com>
2020-08-05 09:57:57 -0300

    registering more languages

--- src/components/CodeRenderer.js
+++ src/components/CodeRenderer.js
@@ -5,22 +5,26 @@ import "highlight.js/styles/srcery.css";
 import bash from "highlight.js/lib/languages/bash";
 import cLike from "highlight.js/lib/languages/c-like";
 import elixir from "highlight.js/lib/languages/elixir";
+import html from "highlight.js/lib/languages/htmlbars";
 import java from "highlight.js/lib/languages/java";
 import javascript from "highlight.js/lib/languages/javascript";
+import markdown from "highlight.js/lib/languages/markdown";
+import php from "highlight.js/lib/languages/php";
 import python from "highlight.js/lib/languages/python";
 import ruby from "highlight.js/lib/languages/ruby";
 
-Lowlight.registerLanguage("bash", bash);
-Lowlight.registerLanguage("c", cLike);
-Lowlight.registerLanguage("cpp", cLike);
-Lowlight.registerLanguage("cs", cLike);
+Lowlight.registerLanguage("bash" && "sh" && "zsh", bash);
+Lowlight.registerLanguage("c" && "cpp" && "csharp", cLike);
 Lowlight.registerLanguage("elixir", elixir);
+Lowlight.registerLanguage("html", html);
 Lowlight.registerLanguage("java", java);
 Lowlight.registerLanguage("js", javascript);
+Lowlight.registerLanguage("md", markdown);
+Lowlight.registerLanguage("php", php);
 Lowlight.registerLanguage("py", python);
 Lowlight.registerLanguage("ruby", ruby);
 
-let langs = ["bash", "c", "cpp", "cs", "elixir", "java", "js", "py", "ruby"];
+let langs = ["bash", "c", "cpp", "cs", "elixir", "java", "js", "php", "py", "ruby", "html"];
 
 function CodeRenderer(props) {
   const { language, ...rest } = props;
