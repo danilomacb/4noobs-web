31c9990100bc82be5dc6cf8039ca2d1c53f19586
danilomacb <danilo.bakun@gmail.com>
2020-08-08 08:30:23 -0300

    registered langs working

--- src/components/CodeRenderer.js
+++ src/components/CodeRenderer.js
@@ -13,8 +13,10 @@ import php from "highlight.js/lib/languages/php";
 import python from "highlight.js/lib/languages/python";
 import ruby from "highlight.js/lib/languages/ruby";
 
-Lowlight.registerLanguage("bash" && "sh" && "zsh", bash);
-Lowlight.registerLanguage("c" && "cpp" && "csharp", cLike);
+Lowlight.registerLanguage("bash", bash);
+Lowlight.registerLanguage("c", cLike);
+Lowlight.registerLanguage("cpp", cLike);
+Lowlight.registerLanguage("csharp", cLike);
 Lowlight.registerLanguage("elixir", elixir);
 Lowlight.registerLanguage("html", html);
 Lowlight.registerLanguage("java", java);
@@ -24,13 +26,12 @@ Lowlight.registerLanguage("php", php);
 Lowlight.registerLanguage("py", python);
 Lowlight.registerLanguage("ruby", ruby);
 
-let langs = ["bash", "c", "cpp", "cs", "elixir", "java", "js", "php", "py", "ruby", "html"];
+let langs = ["bash", "c", "cpp", "cs", "elixir", "html", "java", "js", "md", "php", "py", "ruby"];
 
 function CodeRenderer(props) {
   const { language, ...rest } = props;
-  const found = langs.find((lang) => lang === language);
 
-  return <Lowlight language={found ? language : undefined} {...rest} />;
+  return <Lowlight language={language} {...rest} />;
 }
 
 export default CodeRenderer;
