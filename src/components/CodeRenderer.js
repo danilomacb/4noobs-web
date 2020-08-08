7496a1c2ac36ffeb2127f55bd349549a38db759a
danilomacb <danilo.bakun@gmail.com>
2020-08-08 09:51:43 -0300

    registering more langs

--- src/components/CodeRenderer.js
+++ src/components/CodeRenderer.js
@@ -14,19 +14,48 @@ import python from "highlight.js/lib/languages/python";
 import ruby from "highlight.js/lib/languages/ruby";
 
 Lowlight.registerLanguage("bash", bash);
+Lowlight.registerLanguage("sh", bash);
 Lowlight.registerLanguage("c", cLike);
 Lowlight.registerLanguage("cpp", cLike);
+Lowlight.registerLanguage("c++", cLike);
 Lowlight.registerLanguage("csharp", cLike);
+Lowlight.registerLanguage("cs", cLike);
 Lowlight.registerLanguage("elixir", elixir);
 Lowlight.registerLanguage("html", html);
 Lowlight.registerLanguage("java", java);
+Lowlight.registerLanguage("javascript", javascript);
 Lowlight.registerLanguage("js", javascript);
+Lowlight.registerLanguage("jsx", javascript);
+Lowlight.registerLanguage("markdown", markdown);
 Lowlight.registerLanguage("md", markdown);
 Lowlight.registerLanguage("php", php);
+Lowlight.registerLanguage("python", python);
 Lowlight.registerLanguage("py", python);
 Lowlight.registerLanguage("ruby", ruby);
+Lowlight.registerLanguage("rb", ruby);
 
-let langs = ["bash", "c", "cpp", "cs", "elixir", "java", "js", "md", "php", "py", "ruby"];
+let langs = [
+  "bash",
+  "sh",
+  "c",
+  "cpp",
+  "c++",
+  "csharp",
+  "cs",
+  "elixir",
+  "html",
+  "java",
+  "javascript",
+  "js",
+  "jsx",
+  "markdown",
+  "md",
+  "php",
+  "python",
+  "py",
+  "ruby",
+  "rb",
+];
 
 function CodeRenderer(props) {
   const { language, ...rest } = props;
